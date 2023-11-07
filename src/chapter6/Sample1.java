0e156923a00d4f6b223c3e5cce4170f7cb1bc497
Matayoshi Rikuu <s23024@std.it-college.ac.jp>
2023-11-07 16:55:46 +0900

    chapter6-2まで終了。

---
+++ src/chapter6/Sample1.java
@@ -0,0 +1,8 @@
+package chapter6;
+
+public class Sample1 {
+    public static void main(String[] args) {
+        Tax tax = new Tax(100, "田中宏", 300_000, 100_000);
+        System.out.println(tax.zeigaku());
+    }
+}
