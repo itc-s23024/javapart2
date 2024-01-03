1ac9bbebb4342fd744862a70e24c46dc58da0c97
Matayoshi Rikuu <s23024@std.it-college.ac.jp>
2024-01-03 22:22:28 +0900

    chapter12_2Test6まで終了。１/３(水)

--- src/exercise1/test6.java
+++ src/exercise1/test6.java
@@ -2,6 +2,11 @@ package exercise1;
 
 public class test6 {
     public static void main(String[] args) {
-
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(i * 3 + j + " ");
+            }
+            System.out.println(" ");
+        }
     }
 }
