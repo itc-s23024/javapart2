ab26de0e62002d773386a117829164be3d8b06a4
Matayoshi Rikuu <s23024@std.it-college.ac.jp>
2024-01-25 12:40:39 +0900

    exersice4TangoGameの途中まで終了。１/24(水)

--- src/exercise4/TangoGame.java
+++ src/exercise4/TangoGame.java
@@ -1,22 +1,27 @@
 package exercise4;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class TangoGame {
+
     public static void main(String[] args) {
         //＊変数の定義
         //答え
         //入力値
         //チャレンジできる精神
         //答えを配列に入れる
-        String[] answer_list = {"BOOK", "APPLE", "JAPAN"};
+        //String[] answer_list = {"BOOK", "APPLE", "JAPAN"};
+        String[] answer_list = {"BOOK"};
+        Scanner sc = new Scanner(System.in);
 
         //ランダムに答えを決め、char型配列に変換する
         Random r = new Random();
         int rand_num = r.nextInt(answer_list.length);
         String str = answer_list[rand_num];
         char[] answer_wourd = str.toCharArray();
-        //System.out.println(answer_wourd[0]);
+
+        //正解が入っている配列は　answer_word
 
         // _ で埋めたchar型配列を作る
         char[] guess = new char[answer_wourd.length];
@@ -24,24 +29,74 @@ public class TangoGame {
             guess[i] = '_';
         }
 
-        System.out.println("現在の状態: ");
-        //guessという配列の中身を表示する
 
+        int count = 5;
+        while (count > 0) {
+            System.out.println("現在の状態: ");
+
+            //printArayメソッド名を呼び出す
+            printArray(guess);
+
+            //「1文字を入力してください」と表示して、ユーザーが入力した文字を取得し変数に入れる
+            System.out.println("1文字を入力してください");
+            String s = sc.nextLine();
+            char input = s.charAt(0);
+            input = Character.toUpperCase(input);
+            count--;
+
+            //guess配列を書き換える
+            // _ _ _ _ -> bが入力されたら B _ _ _
+            for (int i = 0; i < answer_wourd.length; i++) {
+                if (answer_wourd[i] == input) {
+                    guess[i] = input;
+                }
+            }
+
+            //checkWinメソッドを使い、判定する
+            //trueが返ってきたら正解ということ
+            if (checkWin(guess)){
+                System.out.println("正解です");
+            }
+
+
+        }
 
         /*
         単語当てゲーム！
         現在の状態:_ _ _ _
         残り回数: 5
         1文字を入力してください
+         a
+         現在の状態:_ _ _ _
+         残り回数:4
+         1文字を入力してください
+         b
          */
 
 
-        //＊どういう機能が必要か　メソッド
-        //答えと入力値が同じ文字だったらtrueを返す
-        //現在の状態を表示する
-        //回数を５回までにする
-        //５回終わったらゲームオーバー
-        //当たってたらゲームクリア
-
     }
+
+    //配列の中身を表示するメソッドを作る
+    //引数は　配列(char型)
+    //printArayメソッド名
+    public static void printArray(char[] array){
+        for (int i=0; i<array.length; i++) {
+            System.out.println(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    //判定するメソッド
+    //引数はguess配列
+    //guess配列の要素にひとつでも_が含まれていたら,falseを返す
+    //_がひとつもなければtrueを返す
+    public static boolean checkWin(char[] array){
+        for (int i=0; i< array.length; i++){
+            if (array[i] == '_'){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
